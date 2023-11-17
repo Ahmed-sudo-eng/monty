2c91f2610ad42643cfb58ca6e83375d4f2d0adb8
Ahmed Omer <ahmedomer695@gmail.com>
2023-11-17 10:22:36 +0200

    main.c file was modified

--- main.c
+++ main.c
@@ -79,29 +79,42 @@ int main(int argc, char *argv[])
 		}
 		else if (opcode[0] == 'p' && opcode[1] == 'a' && opcode[2] == 'l' && opcode[3] == 'l')
 			pall(head);
-		else if (opcode[0] == 'p' && opcode[1] == 'i' && opcode[2] == 'n' && opcode[3] == 't')
+
+		else if (strlen(opcode) == 4 && opcode[0] == 'p' && opcode[1] == 'i' && opcode[2] == 'n' 
+				                                                     && opcode[3] == 't')
 			pint(head, lineNumber);
+
 		else if (opcode[0] == 'p' && opcode[1] == 'o' && opcode[2] == 'p')
 			pop(&head, lineNumber);
+
 		else if (opcode[0] == 's' && opcode[1] == 'w' && opcode[2] == 'a' && opcode[3] == 'p')
 			swap(head, lineNumber);
+
 		else if (opcode[0] == 'a' && opcode[1] == 'd' && opcode[2] == 'd')
 			add(head, lineNumber);
+
 		else if (opcode[0] == 's' && opcode[1] == 'u' && opcode[2] == 'b')
 			sub(head, lineNumber);
+
 		else if (opcode[0] == 'd' && opcode[1] == 'i' && opcode[2] == 'v')
 			divi(head, lineNumber);
+
 		else if (opcode[0] == 'm' && opcode[1] == 'u' && opcode[2] == 'l')
 			mul(head, lineNumber);
+
 		else if (opcode[0] == 'm' && opcode[1] == 'o' && opcode[2] == 'd')
 			mod(head, lineNumber);
+
 		else if (opcode[0] == 'p' && opcode[1] == 's' && opcode[2] == 't' && opcode[3] == 'r')
 			pstr(head);
+
 		else if (opcode[0] == 'r' && opcode[1] == 'o' && opcode[2] == 't' && opcode[3] == '1')
 			rot1(&head);
+
 		else if (opcode[0] == 'p' && opcode[1] == 'c' && opcode[2] == 'h' && opcode[3] == 'a'
 					  && opcode[4] == 'r')
 			pchar(head, lineNumber);
+
 		else if (opcode[0] == 'n' && opcode[1] == 'o' && opcode[2] == 'p')
 		{
 			lineNumber++;
